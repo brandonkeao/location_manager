8b0e35df97d39de52a39f6374cfbbb6dbe7554a3
brandonkeao <brandon.keao@gmail.com>
2017-06-06 12:35:16 -0400

    Updated styles and setup the main structure of App.js along with initial class method binding

--- src/App.js
+++ src/App.js
@@ -1,21 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
 import './App.css';
+import 'react-bootstrap';
+import 'css-type-base';
 
 class App extends Component {
+  
+  constructor() {
+    super();
+    // Set initial state values
+    this.state = {
+      favorites: this.setFavorites(),
+      currentPlace: "Mombasa, Kenya",
+      coords: {
+        lat: -4.05,
+        lng: 39.666667
+      }
+    }
+  } // closes constructor()
+
+  setFavorites() {
+    let favorites = [];
+    if (localStorage.favorites) {
+      favorites = JSON.parse(localStorage.favorites);
+    }
+    return favorites;
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div className="heading">
+          <h2>Welcome to Location Manager</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Placeholder for future components...
         </p>
       </div>
     );
-  }
-}
+  } // closes render()
+
+} // closes Component 
 
 export default App;
